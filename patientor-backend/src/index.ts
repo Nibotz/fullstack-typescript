92b516000488081f4012e4fc2a014681a08ad9c0
Niilo Mikkola <niilo.mikkola@gmail.com>
2023-12-15 17:24:58 +0200

    better file structure

--- patientor-backend/src/index.ts
+++ patientor-backend/src/index.ts
@@ -4,7 +4,6 @@ import patientRouter from './routes/patients';
 import cors from 'cors';
 const app = express();
 app.use(express.json());
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 app.use(cors());
 
 app.get('/api/ping', (_req, res) => {
